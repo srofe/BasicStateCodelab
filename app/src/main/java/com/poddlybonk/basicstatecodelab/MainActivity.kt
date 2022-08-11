8b8ce831b02edfa0dc6b982221c1790ba424122b
Simon Rofe <simon.rofe@ieee.org>
2022-08-11 13:02:29 +1000

    Update to use WellnessScreen as main screen

--- app/src/main/java/com/poddlybonk/basicstatecodelab/MainActivity.kt
+++ app/src/main/java/com/poddlybonk/basicstatecodelab/MainActivity.kt
@@ -19,22 +19,9 @@ class MainActivity : ComponentActivity() {
             BasicStateCodelabTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    WellnessScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BasicStateCodelabTheme {
-        Greeting("Android")
     }
 }
